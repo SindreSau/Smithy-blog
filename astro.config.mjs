5e7ede5798161d8cdc5aed985a9212c93ad0cb52
Sindre Sauarlia <101213882+SindreSau@users.noreply.github.com>
2024-04-29 15:17:20 +0200

    Add @astrojs/vercel and @vercel/analytics packages, and update astro.config.mjs with Vercel adapter and web analytics settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import solidJs from '@astrojs/solid-js';
+import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://smithy-blog.vercel.app/",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({
-    applyBaseStyles: false
-  }), react()]
+    site: 'https://smithy-blog.vercel.app/',
+    integrations: [
+        mdx(),
+        sitemap(),
+        solidJs(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        react(),
+    ],
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: { enabled: true },
+    }),
 });
